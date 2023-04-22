d401688f489b1c5fe36ef96f7ac9139d45938e7f
steverova <steverova0594@gmail.com>
2023-04-22 01:06:02 -0600

    vercel

--- src/api/task.api.js
+++ src/api/task.api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const taskApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000/task/api/v1/tasks/'
-    // baseURL: 'https://sunsetsteve.pythonanywhere.com/task/api/v1/tasks/'
+    // baseURL: 'http://127.0.0.1:8000/task/api/v1/tasks/'
+    baseURL: 'https://sunsetsteve.pythonanywhere.com/task/api/v1/tasks/'
 })
 
 export function getAllTask(){
